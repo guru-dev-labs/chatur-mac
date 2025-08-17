a8c134d3afdccc2fca0b9232208cd78bec827e07
Deevanshu <deevanshu0@gmail.com>
2025-08-17 12:08:41 +0530

    Initial Commit

---
+++ chatur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chatur
+//
+//  Created by Deevanshu Guru on 17/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chatur/chaturApp.swift
@@ -0,0 +1,17 @@
+//
+//  chaturApp.swift
+//  chatur
+//
+//  Created by Deevanshu Guru on 17/08/25.
+//
+
+import SwiftUI
+
+@main
+struct chaturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
